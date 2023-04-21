2136f083acdb961d907c020c51734052739638f7
Itai Spiegel <itai.spiegel@gmail.com>
2023-04-21 22:02:33 +0300

    Implement message_slot

--- message_slot.c
+++ message_slot.c
@@ -1,48 +1,133 @@
+#include <linux/fs.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/radix-tree.h>
 #include <linux/slab.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
 
 #include "message_slot.h"
+
+struct file_data {
+    unsigned int channel_id;
+    unsigned int minor;
+};
+
+struct channel {
+    unsigned int message_size;
+    char message[BUFFER_SIZE];
+};
+
+/* An array of radix trees, which holds a tree for each minor.
+ Each tree is a mapping between the channel id and it's relevant channel struct.
+ */
+static struct radix_tree_root **channels_trees;
+
+static struct channel *get_channel_from_file(struct file *file) {
+    unsigned int minor = iminor(file->f_inode);
+    struct radix_tree_root *channels_tree = channels_trees[minor];
+    struct file_data *fdata = file->private_data;
+    return radix_tree_lookup(channels_tree, fdata->channel_id);
+}
+
+static void free_radix_tree(struct radix_tree_root *root) {
+    struct radix_tree_iter iter;
+    void **slot;
+
+    radix_tree_for_each_slot(slot, root, &iter, 0) { kfree(*slot); }
+}
 
 int device_open(struct inode *inode, struct file *file) {
     struct file_data *fdata =
         (struct file_data *)kmalloc(sizeof(struct file_data), GFP_KERNEL);
     if (fdata == NULL) {
+        printk(KERN_ERR "Couldn't allocate message slot file descriptor");
         return 1;
     }
+
     fdata->channel_id = 0;
+    fdata->minor = iminor(inode);
     file->private_data = fdata;
+
+    // TODO Release
+    if (channels_trees[fdata->minor] == NULL) {
+        channels_trees[fdata->minor] = (struct radix_tree_root *)kmalloc(
+            sizeof(struct radix_tree_root), GFP_KERNEL);
+        if (channels_trees[fdata->minor] == NULL) {
+            printk(KERN_ERR "Couldn't allocate radix tree for message slot");
+            return 1;
+        }
+
+        INIT_RADIX_TREE(channels_trees[fdata->minor], GFP_KERNEL);
+    }
+
+    printk(KERN_INFO "Opened message slot file");
     return 0;
 }
 
 long device_ioctl(struct file *file, unsigned int cmd, unsigned long param) {
+    struct file_data *fdata = (struct file_data *)file->private_data;
+    struct radix_tree_root *channels_tree;
+    struct channel *channel;
+
     if (cmd != MSG_SLOT_CHANNEL || param == 0) {
         return -EINVAL;
     }
 
-    struct file_data *fdata = file->private_data;
     fdata->channel_id = (unsigned int)param;
+
+    channels_tree = channels_trees[fdata->minor];
+    if (radix_tree_lookup(channels_tree, fdata->channel_id) == NULL) {
+        channel = (struct channel *)kmalloc(sizeof(struct channel), GFP_KERNEL);
+        radix_tree_insert(channels_tree, fdata->channel_id, channel);
+    }
+
     return 0;
 }
 
 ssize_t device_read(struct file *file, char __user *buffer, size_t length,
                     loff_t *offset) {
-    struct file_data *fdata = file->private_data;
+    struct file_data *fdata = (struct file_data *)file->private_data;
+    struct channel *channel;
+
     if (fdata->channel_id == 0) {
         return -EINVAL;
     }
     if (length == 0 || length > BUFFER_SIZE) {
         return -EMSGSIZE;
     }
-    return 0;
+
+    channel = get_channel_from_file(file);
+    if (channel->message_size == 0) {
+        return -EWOULDBLOCK;
+    }
+    if (length < channel->message_size) {
+        return -ENOSPC;
+    }
+
+    if (copy_to_user(buffer, channel->message, channel->message_size) > 0) {
+        return -EFAULT;
+    }
+
+    return channel->message_size;
 }
 ssize_t device_write(struct file *file, const char __user *buffer,
                      size_t length, loff_t *offset) {
     struct file_data *fdata = file->private_data;
+    struct channel *channel;
     if (fdata->channel_id == 0) {
         return -EINVAL;
     }
-    return 0;
+    if (length == 0 || length > BUFFER_SIZE) {
+        return -EMSGSIZE;
+    }
+
+    channel = get_channel_from_file(file);
+    if (copy_from_user(channel->message, buffer, length) > 0) {
+        return -EFAULT;
+    }
+    channel->message_size = length;
+    return length;
 }
 
 int device_release(struct inode *inode, struct file *file) {
@@ -50,16 +135,38 @@ int device_release(struct inode *inode, struct file *file) {
     return 0;
 }
 
+static const struct file_operations fops = {.owner = THIS_MODULE,
+                                            .open = device_open,
+                                            .unlocked_ioctl = device_ioctl,
+                                            .read = device_read,
+                                            .write = device_write,
+                                            .release = device_release};
+
 int init_module(void) {
-    printk(KERN_INFO "Registering message slot\n");
     if (register_chrdev(MESSAGE_SLOT_DEVICE_MAJOR, MESSAGE_SLOT_DEVICE_NAME,
                         &fops) < 0) {
         printk(KERN_ERR "Error registering message_slot\n");
+        return 1;
     }
+
+    channels_trees = (struct radix_tree_root **)kcalloc(
+        MINORS_COUNT, sizeof(struct radix_tree_root **), GFP_KERNEL);
+    if (channels_trees == NULL) {
+        printk(KERN_ERR "Error initializing channels trees");
+        return 1;
+    }
+
+    printk(KERN_INFO "Registered message slot\n");
     return 0;
 }
 
 void cleanup_module(void) {
-    printk(KERN_INFO "Unregistering message slot\n");
+    size_t i;
     unregister_chrdev(MESSAGE_SLOT_DEVICE_MAJOR, MESSAGE_SLOT_DEVICE_NAME);
+    printk(KERN_INFO "Unregistered message slot\n");
+
+    for (i = 0; i < MINORS_COUNT; i++) {
+        free_radix_tree(channels_trees[i]);
+    }
+    kfree(channels_trees);
 }
--- message_slot.h
+++ message_slot.h
@@ -1,36 +1,12 @@
 #ifndef MESSAGE_SLOT_H
 #define MESSAGE_SLOT_H
 
-#include <linux/fs.h>
-#include <linux/types.h>
+#include <linux/ioctl.h>
 
 #define MESSAGE_SLOT_DEVICE_MAJOR 235
 #define MESSAGE_SLOT_DEVICE_NAME "message_slot"
 #define MSG_SLOT_CHANNEL _IOW(MESSAGE_SLOT_DEVICE_MAJOR, 0, unsigned int)
 #define BUFFER_SIZE 128
-
-struct file_data {
-    unsigned int channel_id;
-};
-
-struct channel {
-    unsigned int message_size;
-    char message[BUFFER_SIZE];
-};
-
-int device_open(struct inode *inode, struct file *file);
-long device_ioctl(struct file *file, unsigned int cmd, unsigned long param);
-ssize_t device_read(struct file *file, char __user *buffer, size_t length,
-                    loff_t *offset);
-ssize_t device_write(struct file *file, const char __user *buffer,
-                     size_t length, loff_t *offset);
-int device_release(struct inode *inode, struct file *file);
-
-const struct file_operations fops = {.owner = THIS_MODULE,
-                                     .open = device_open,
-                                     .unlocked_ioctl = device_ioctl,
-                                     .read = device_read,
-                                     .write = device_write,
-                                     .release = device_release};
+#define MINORS_COUNT 256
 
 #endif
