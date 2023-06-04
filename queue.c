8ebf64c5509571bebf85e0c3d7f7cc336b428645
Itai Spiegel <itai.spiegel@gmail.com>
2023-06-04 22:19:43 +0300

    Initial commit

---
+++ queue.c
@@ -0,0 +1,29 @@
+#include "queue.h"
+
+void initQueue(void) {
+}
+
+void destroyQueue(void) {
+}
+
+void enqueue(void *item) {
+}
+
+void *dequeue(void) {
+}
+
+bool tryDequeue(void **item_ptr) {
+    return false;
+}
+
+size_t size(void) {
+    return 0;
+}
+
+size_t waiting(void) {
+    return 0;
+}
+
+size_t visited(void) {
+    return 0;
+}
